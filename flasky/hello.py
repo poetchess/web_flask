36c6dcf263e19e8606a70c0323efcac3d5137270
poetchess <ilvbaicai@126.com>
2017-03-10 23:43:05 +0800

    Adding a dynamic route

--- flasky/hello.py
+++ flasky/hello.py
@@ -13,5 +13,13 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+#The portion enclosed in angle brackets is the dynamic part.
+#Dynamic components in routes are strings by default.
+#When the view function is invoked, Flask sends the dynamic component as an
+#argument.
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 if __name__ == '__main__':
     app.run(debug=True)
