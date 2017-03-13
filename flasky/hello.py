a35fd2d1ea95d21b0f9f67e1d96c88aa1b8f057b
poetchess <ilvbaicai@126.com>
2017-03-13 23:12:46 +0800

    Using templates with flask-bootstrap

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 '''
 A Flask application must create an application instance.
@@ -9,6 +10,7 @@ handling, using WSGI protocol.
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 #The association b/w a URL and the function that handles it is called a route.
 #Functions like 'index()' are called view functions.
@@ -22,7 +24,7 @@ def index():
 #argument.
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', nameholder=name)
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()
