28cccea04327b5a88fa85bb6ee69bed9a3661869
poetchess <ilvbaicai@126.com>
2017-03-17 00:02:25 +0800

    Web forms with flask-wtf

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 '''
 A Flask application must create an application instance.
 Web server passes all requests it receives from clients to this object for 
 handling, using WSGI protocol.
 '''
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string, really?'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 #The association b/w a URL and the function that handles it is called a route.
 #Functions like 'index()' are called view functions.
-@app.route('/')
+
+#'methods' argument allows Flask to register the view function as a handler
+#for GET and POST requests.
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+
+    #validate_on_submit() returns True if the form was submitted and the data 
+    #has been accepted by all the field validators.
+    if form.validate_on_submit():
+        name = form.name.data
+        #clear the form data. Otherwise, it will be rendered on the page.
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 #The portion enclosed in angle brackets is the dynamic part.
 #Dynamic components in routes are strings by default.
