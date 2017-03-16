d7a6c5abb778da9f2bad3165ea9804f913ba3efc
poetchess <ilvbaicai@126.com>
2017-03-16 12:34:01 +0800

    Adding dates and times with flask-moment

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
 '''
 A Flask application must create an application instance.
 Web server passes all requests it receives from clients to this object for 
@@ -11,12 +12,13 @@ app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 #The association b/w a URL and the function that handles it is called a route.
 #Functions like 'index()' are called view functions.
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 #The portion enclosed in angle brackets is the dynamic part.
 #Dynamic components in routes are strings by default.
