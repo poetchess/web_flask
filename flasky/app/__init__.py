27c265116c61a0f3a2c152d9e6a8f43dd15dcccc
poetchess <ilvbaicai@126.com>
2017-08-05 17:01:08 +0800

    large application structure

---
+++ flasky/app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # The configuration settings can be imported into the application using
+    # from_object() method.
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    # An application is created and configured, the extensions can be initialized.
+    # Calling init_app() on the extensions completes their initialization.
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # Attach routes and custom error pages here.
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
