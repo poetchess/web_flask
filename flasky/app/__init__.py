a28d20f60f0b9191df3cd8f724fee965a413b32f
poetchess <ilvbaicai@126.com>
2017-08-20 15:40:36 +0800

    User login

--- flasky/app/__init__.py
+++ flasky/app/__init__.py
@@ -3,12 +3,20 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+# 'session_protection' can be None, 'basic' or 'strong'
+login_manager.session_protection = 'strong'
+# 'login_view' sets the endpoint for the login page. Since the login route is
+# inside a blueprint, it needs to be prefix with the blueprint name.
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -24,6 +32,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Attach routes and custom error pages here.
     from main import main as main_blueprint
