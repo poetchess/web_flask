cb10b60b6b409780f7de1ce05b38140ce2dddeec
poetchess <ilvbaicai@126.com>
2017-08-22 15:47:25 +0800

    Account confirmation

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
-from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..models import User
+from ..email import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,6 +39,56 @@ def register():
                     password=form.password.data)
 
         db.session.add(user)
-        flash('You can now login')
-        return redirect(url_for('auth.login'))
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                  'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
+
+
+
+# This route is protected with 'login_required' to ensure that when it is
+# accessed, the user that is making the request is known.
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+# From a blueprint, the 'before_request' hook applies only to requests that
+# belong to the blueprint. To install a hook for all application requests from
+# a blueprint, use'before_app_request'
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+        and not current_user.confirmed \
+        and request.endpoint \
+        and request.endpoint[:5] != 'auth.' \
+        and request.endpoint != 'static':
+       return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect('main.index')
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you.')
+    return redirect(url_for('main.index'))
